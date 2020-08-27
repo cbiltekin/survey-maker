aa9e67c2b9342dd46ecbfdd6a491043e04256694
cbiltekin <cemre.biltekin@gmail.com>
2020-08-27 04:01:38 +0300

    answering survey page done

--- frontend/src/components/AnswerTextView.js
+++ frontend/src/components/AnswerTextView.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { Input } from 'antd';
 
-const AnswerTextView = () => {
+const AnswerTextView = (props) => {
+    const [question, setQuestion] = useState({});
+    const { TextArea } = Input;
+    const [value, setValue] = useState();
+
+    useEffect( () => {
+        setQuestion(props.question);
+    },[props.question]);
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+        console.log(value); //bir adım geriden geliyor
+    }
+
     return (
         <div>
-            
+        {question.name}
+        <div><TextArea rows={4} onChange={handleChange} value={value} /></div>
         </div>
     );
 };
